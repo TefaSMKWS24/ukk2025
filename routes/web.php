5cd5bb286df754a182b89b9b908cfab3a320dab2
Chanif2003 <53119919+Chanif2003@users.noreply.github.com>
2025-02-01 10:47:44 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KasirController;
 use App\Http\Controllers\KategoriController;
@@ -23,8 +26,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('barang', BarangController::class);
-Route::resource('kasir', KasirController::class);
-Route::resource('kategori', KategoriController::class);
-Route::resource('transaksi', TransaksiController::class);
-Route::resource('pelanggan', PelangganController::class);
+
+//GUEST (SEBELUM LOGIN)
+Route::middleware(['guest:kasir'])->group(function(){
+    Route::get('/kasir', function(){ return view('auth.loginkasir'); })->name('loginkasir');
+    Route::post('/loginkasir', [AuthController::class,'loginkasir']);
+});
+Route::middleware(['guest:admin'])->group(function(){
+    Route::get('/admin', function(){ return view('auth.loginadmin'); })->name('loginadmin');
+    Route::post('/loginadmin', [AuthController::class,'loginadmin']);
+});
+
+
+//AUTH (SETELAH LOGIN)
+Route::middleware('auth:kasir')->group(function(){
+    Route::get('/kasir/dashboard', [DashboardKasirController::class, 'dashboard']);
+    Route::get('/kasir/logout',  [AuthController::class, 'logoutkasir']);
+});
+
+Route::middleware('auth:admin')->group(function(){
+    Route::get('/admin/dashboard', [DashboardAdminController::class, 'dashboard']);
+    Route::get('/admin/logout',  [AuthController::class, 'logoutkasir']);
+
+    Route::resource('barang', BarangController::class);
+    Route::resource('kasir', KasirController::class);
+    Route::resource('kategori', KategoriController::class);
+    Route::resource('transaksi', TransaksiController::class);
+    Route::resource('pelanggan', PelangganController::class);
+
+});
